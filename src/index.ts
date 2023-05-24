a1e9f5f77d5483acccd8520b2e485cc1d230f98b
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-05-24 22:49:53 +0200

    Rebase of the project

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,74 @@
-/**
- * type : the token group
- * selector : regexp to match the token
- * meta : in case your selection is more complex, 
- *        you can edit it with the meta parameter
- */
-export interface TokenRule {
-    type: String,
-    selector: RegExp,
-    meta: Function
+interface Token {
+    Name: string,           /* The token name */
+    Category: Array<string> /* Categories can be used if the same
+                            token is used in different context for
+                            example the `<` token for the begining 
+                            of a jsx depend on the grammar. An 
+                            another example can be a object 
+                            property named like a keyword.*/
+    Indent: number, /* The indentation value */
+    Value: string,  /* The value of the token */
+    SOL: boolean    /* True if is the first token of the line */
+    EOL: boolean    /* True if is the last token of the line */
 }
 
-export function default_meta(match: RegExp) {
-    return match;
+interface TokenMatch {
+    Name: string,       // The token name
+    Selector: RegExp,   // Regular expression to match the token
+    Category: Array<string>
 }
 
-export function tokenize(source: string, rules: Array<TokenRule>): Array<Object> {
-    let tokens: Array<Object> = [];
+interface TokenizerConfig {
+    TabIndentConst?: number,
+    Matchers: Array<TokenMatch>
+}
 
-    while (source != "") {
-        let flag = false;
-        let closest: any = null, diff: any = source.length + 1;
+function tokenize(source: string, config: TokenizerConfig): Array<Token> {
+    let tokens: Array<Token> = [];
 
-        for (let i = 0; i < rules.length; i++) {
-            let match: any = rules[i].selector.exec(source);
+    // the carret is the pointer of the current 
+    // position of tokenizer in the source string
+    let carret = 0;
 
-            // continue if there is no match
-            if (match == null)
-                continue;
+    let eol, sol = true;
 
-            match = {reg: match[0], index: match.index};
-            match = rules[i].meta(match)
-
-            if (match == null)
-                continue;
-
-            if (match.index == 0) {
-                tokens.push({ type: rules[i].type, token: match.reg });
-
-                source = source.substring(match.reg.length);
-                flag = true;
-                break;
-            }
-
-            if (diff > match.index) {
-                diff = match.index;
-                closest = { type: rules[i].type, token: match.reg }
-            }
+    for (;carret < source.length; carret++) {
+        let indent = 0;
+        while (1) {
+            if (source[carret] == ' ') indent++;
+            else if (source[carret] == '\t') indent += 4;
+            else if (source[carret] == '\n') { indent = 0; sol = true; }
+            else break;
+            carret++;
         }
 
-        if (!flag) {
-            if (closest != null) {
-                tokens.push({ type: "UNDEFINED", token: source.substring(0, diff) });
-                tokens.push(closest);
-                source = source.substring(diff+closest.token.length);
-            } else {
-                tokens.push({ type: "UNDEFINED", token: source });
+        let r: any = null, idx: number = -1;
+        for (let i = 0; i < config.Matchers.length; i++) {
+            r = config.Matchers[i].Selector.exec(source);
+            if (r && r.index == 0) {
+                idx = i;
                 break;
             }
         }
+
+        if (!r && carret < source.length) { throw new Error("Unexpected token"); }
+        else {
+            if (sol && tokens.length > 0) {
+                tokens[-1].EOL = true;
+            }
+
+            tokens.push({
+                Name: config.Matchers[idx].Name,
+                Category: config.Matchers[idx].Category,
+                Indent: indent,
+                Value: r[0],
+                SOL: sol,
+                EOL: false
+            })
+        }
+
+        carret += r[0];
+        sol = false;
     }
 
     return tokens;
